61fed7f6e1aa5899eed556a082d1da06d04685e8
Dmartins2695 <62670499+Dmartins2695@users.noreply.github.com>
2024-12-05 10:02:30 +0000

    working styles

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require('expo/metro-config')
+const { withNativeWind } = require('nativewind/metro')
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname)
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = withNativeWind(config, { input: './global.css' })
+
+const path = require('path')
+config.resolver.disableHierarchicalLookup = true
+config.resolver.nodeModulesPaths = [path.resolve(__dirname, 'node_modules')]
