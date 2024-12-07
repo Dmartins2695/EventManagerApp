4fead9e13aee259961b0cc8a3a03b50e74e25905
Dmartins2695 <62670499+Dmartins2695@users.noreply.github.com>
2024-12-07 00:05:22 +0000

    add snackbar provider to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,23 +3,22 @@ import '../global.css'
 import { PaperProvider } from 'react-native-paper'
 import { Stack } from 'expo-router'
 import { lightTheme } from '../theme'
-import { View } from 'react-native'
 import React from 'react'
-import { verifyInstallation } from 'nativewind'
-import { TextField } from '@/components/WrappedComponents'
+import { SnackbarProvider } from '@/components/SnackbarProvider'
 
 export default function RootLayoutNav() {
-
   return (
     <PaperProvider theme={lightTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="auth/signin" />
-        <Stack.Screen name="auth/signup" />
-        <Stack.Screen name="user/dashboard" />
-        {/*<Stack.Screen name="forgot-password" />
+      <SnackbarProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="auth/signin" />
+          <Stack.Screen name="auth/signup" />
+          <Stack.Screen name="user/dashboard" />
+          {/*<Stack.Screen name="forgot-password" />
           <Stack.Screen name="create-password" />
           */}
-      </Stack>
+        </Stack>
+      </SnackbarProvider>
     </PaperProvider>
   )
 }
